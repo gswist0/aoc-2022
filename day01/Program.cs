51e4bd377943db1422fed1ee278c4b73d919bb68
gswist0 <72512234+gswist0@users.noreply.github.com>
2022-12-01 15:28:00 +0100

    day1

---
+++ day01/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+using System.Collections;
+
+string[] lines = File.ReadAllLines("input.txt");
+
+List<int> sums = lines.Aggregate((new List<int>(), 0),
+    (tuple, next) => {
+        if ( next == "" ){
+            tuple.Item1.Add(tuple.Item2);
+            tuple.Item2 = 0;
+        }else{
+            tuple.Item2 += Int32.Parse(next);
+        }
+        return tuple;
+    },
+    result => result.Item1
+);
+
+Console.WriteLine(sums.Max());
+
+List<int> arr_sums = sums;
+
+int sum3 = arr_sums.OrderByDescending(s=>s).Take(3).Sum();
+
+Console.WriteLine(sum3);
